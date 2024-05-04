f19999f50f3a8bab6ff5ae54b0ef68ea3725a6b1
xl000 <l_xb@foxmail.com>
2024-05-05 01:44:22 +0800

    feature: concurrency matrix multiply

--- src/matrix.rs
+++ src/matrix.rs
@@ -3,10 +3,14 @@ use anyhow::{bail, Result};
 use std::{
     fmt::{self, Debug, Display},
     ops::{Add, Mul},
+    sync::mpsc,
+    thread,
 };
 
 // 1. matrix multiplication and unit tests
 // 2. thread one-shot channel
+
+const THREAD_COUNT: usize = 4;
 
 pub struct Matrix<T> {
     data: Vec<T>,
@@ -24,16 +28,52 @@ impl<T> Matrix<T> {
     }
 }
 
+struct MsgInput<T> {
+    idx: usize,
+    row: Vector<T>,
+    col: Vector<T>,
+}
+
+struct MsgOutput<T> {
+    idx: usize,
+    val: T,
+}
+
+struct Msg<T> {
+    input: MsgInput<T>,
+    sender: oneshot::Sender<MsgOutput<T>>,
+}
+
 pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> Result<Matrix<T>>
 where
-    T: Mul<Output = T> + Add<Output = T> + Copy + Default,
+    T: Mul<Output = T> + Add<Output = T> + Copy + Default + Send + 'static,
 {
     if a.col != b.row {
         bail!("invalid matrix size: a.col-{} != b.row-{}", a.col, b.row);
     }
+
+    let senders = (0..THREAD_COUNT)
+        .map(|_| {
+            let (tx, rx) = mpsc::channel::<Msg<T>>();
+            thread::spawn(move || {
+                for msg in rx {
+                    let val = dot_product(&msg.input.row, &msg.input.col)?;
+                    if let Err(e) = msg.sender.send(MsgOutput {
+                        idx: msg.input.idx,
+                        val,
+                    }) {
+                        eprintln!("send error: {e:?}");
+                    }
+                }
+                Ok::<_, anyhow::Error>(())
+            });
+            tx
+        })
+        .collect::<Vec<_>>();
+
     // a row vector dot b col vector
     let matrix_len = a.row * b.col;
-    let mut data = vec![T::default(); matrix_len];
+    let mut receivers = Vec::with_capacity(matrix_len);
     for i in 0..a.row {
         for j in 0..b.col {
             let a_row_vector = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
@@ -44,15 +84,32 @@ where
                 .collect::<Vec<_>>();
             let b_col_vector = Vector::new(b_col_data);
             let matrix_idx = i * b.col + j;
-            data[matrix_idx] = dot_product(&a_row_vector, &b_col_vector)?;
+            let (tx, rx) = oneshot::channel();
+            senders[matrix_idx % THREAD_COUNT]
+                .send(Msg {
+                    input: MsgInput {
+                        idx: matrix_idx,
+                        row: a_row_vector,
+                        col: b_col_vector,
+                    },
+                    sender: tx,
+                })
+                .unwrap();
+            receivers.push(rx);
         }
     }
+    let mut data = vec![T::default(); matrix_len];
+    for rx in receivers {
+        let MsgOutput { idx, val } = rx.recv()?;
+        data[idx] = val;
+    }
+
     Ok(Matrix::new(data, a.row, b.col))
 }
 
 impl<T> Mul for Matrix<T>
 where
-    T: Mul<Output = T> + Add<Output = T> + Copy + Default,
+    T: Mul<Output = T> + Add<Output = T> + Copy + Default + Send + 'static,
 {
     type Output = Matrix<T>;
 
