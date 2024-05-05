8877aa8fce2fb64cbbeece688042199fb2f0112d
xl000 <l_xb@foxmail.com>
2024-05-05 17:51:12 +0800

    feature: metrics with RwLock<HashMap>

---
+++ src/metrics.rs
@@ -0,0 +1,132 @@
+use anyhow::{anyhow, Result};
+use std::{
+    collections::HashMap,
+    ops::Deref,
+    sync::{Arc, RwLock},
+};
+
+///
+/// concurrency metrics table
+/// inc/dec/snaphot
+///
+/// - Arc<RwLock<HashMap<K, V>>>>
+/// - DashMap
+/// - AtomicXXX
+/// - 共享内存处理, Send/Sync
+/// - fearless concurrency
+///
+
+type MetricsTable = RwLock<HashMap<String, i64>>;
+
+pub struct MetricsState {
+    pub table: MetricsTable,
+}
+
+#[derive(Clone)]
+pub struct Metrics(Arc<MetricsState>);
+
+impl Deref for Metrics {
+    type Target = MetricsState;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Default for Metrics {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Metrics {
+    pub fn new() -> Self {
+        Self(Arc::new(MetricsState {
+            table: MetricsTable::default(),
+        }))
+    }
+
+    pub fn inc(&self, key: impl Into<String>) -> Result<i64> {
+        let key = key.into();
+        let mut table = self.table.write().map_err(|e| anyhow!(e.to_string()))?;
+        let counter = table.entry(key).or_insert(0);
+        *counter += 1;
+        Ok(*counter)
+    }
+
+    pub fn dec(&self, key: impl Into<String>) -> Result<i64> {
+        let key = key.into();
+        let mut table = self.table.write().map_err(|e| anyhow!(e.to_string()))?;
+        let counter = table.entry(key).or_insert(0);
+        *counter -= 1;
+        Ok(*counter)
+    }
+
+    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
+        Ok(self
+            .table
+            .read()
+            .map_err(|e| anyhow!(e.to_string()))?
+            .clone())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{thread, time::Duration};
+
+    use super::*;
+
+    #[test]
+    fn test_metrics() {
+        let metrics = Metrics::new();
+
+        let mut incs = Vec::new();
+        for _ in 0..6 {
+            let metrics = metrics.clone();
+            incs.push(thread::spawn(move || {
+                for _ in 0..20 {
+                    metrics.inc("test").unwrap();
+                    thread::sleep(Duration::from_millis(rand::random::<u64>() % 50 + 20));
+                }
+            }));
+        }
+
+        let mut decs = Vec::new();
+        for _ in 0..4 {
+            let metrics = metrics.clone();
+            decs.push(thread::spawn(move || {
+                for _ in 0..20 {
+                    metrics.dec("test").unwrap();
+                    thread::sleep(Duration::from_millis(rand::random::<u64>() % 50 + 20));
+                }
+            }));
+        }
+
+        let mut readers = Vec::new();
+        for _ in 0..20 {
+            let metrics = metrics.clone();
+            readers.push(thread::spawn(move || {
+                for _ in 0..20 {
+                    thread::sleep(Duration::from_millis(rand::random::<u64>() % 50 + 30));
+                    assert!(metrics.snapshot().is_ok());
+                }
+            }));
+        }
+
+        for inc in incs {
+            inc.join().unwrap();
+        }
+        for dec in decs {
+            dec.join().unwrap();
+        }
+        for reader in readers {
+            reader.join().unwrap();
+        }
+
+        let snapshot = metrics.snapshot();
+        assert!(snapshot.is_ok());
+        let snapshot = snapshot.unwrap();
+        assert_eq!(snapshot.get("test"), Some(&40));
+    }
+}
